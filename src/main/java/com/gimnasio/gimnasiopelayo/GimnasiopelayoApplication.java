71ce25baed5205c2e5587d51c506dad4dee50fb9
Pelayus <rodriguezalvarezpelayo30@gmail.com>
2025-03-25 10:11:17 +0100

    Commit inicial

---
+++ src/main/java/com/gimnasio/gimnasiopelayo/GimnasiopelayoApplication.java
@@ -0,0 +1,13 @@
+package com.gimnasio.gimnasiopelayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasiopelayoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasiopelayoApplication.class, args);
+	}
+
+}
