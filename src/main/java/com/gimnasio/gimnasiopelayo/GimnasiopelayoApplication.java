1930928e84a38eca4c2032d15571d432daeb85f2
Pelayus <rodriguezalvarezpelayo30@gmail.com>
2025-03-28 09:14:20 +0100

    Implementadas las dependencias correspondientes a Java FX, el cual vamos a usar para la parte visual de la aplicación Configuración para ejecutar la primera aplicacion basica con algo viual(menu principal) y poder interactuar con ello

--- src/main/java/com/gimnasio/gimnasiopelayo/GimnasiopelayoApplication.java
+++ src/main/java/com/gimnasio/gimnasiopelayo/GimnasiopelayoApplication.java
@@ -1,13 +1,15 @@
 package com.gimnasio.gimnasiopelayo;
 
-import org.springframework.boot.SpringApplication;
+import com.gimnasio.gimnasiopelayo.presentacion.SistemaClientesFX;
+import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GimnasiopelayoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GimnasiopelayoApplication.class, args);
+		//SpringApplication.run(GimnasiopelayoApplication.class, args);
+		Application.launch(SistemaClientesFX.class,args);
 	}
 
 }
---
+++ src/main/java/com/gimnasio/gimnasiopelayo/presentacion/SistemaClientesFX.java
@@ -0,0 +1,41 @@
+package com.gimnasio.gimnasiopelayo.presentacion;
+
+import com.gimnasio.gimnasiopelayo.GimnasiopelayoApplication;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
+
+public class SistemaClientesFX extends Application {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public void init() {
+        this.applicationContext = new SpringApplicationBuilder(GimnasiopelayoApplication.class).run();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(GimnasiopelayoApplication.class.getResource("/templates/menuprincipal.fxml"));
+        loader.setControllerFactory((applicationContext::getBean));
+        Scene escena = new Scene(loader.load());
+        stage.setTitle("Gimnasio Pelayo");
+        stage.setResizable(false);
+        stage.setScene(escena);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        applicationContext.close();
+        Platform.exit();
+    }
+}
